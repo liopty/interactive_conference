e95cec2c6c6f8bd2d004b3ff176ced6c5c060f37
Maxime <clairand.maxime@gmail.com>
2019-01-09 08:50:06 +0100

    Reglage du bug

--- server.js
+++ server.js
@@ -75,7 +75,9 @@ var ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité 
 
   // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
   socket.on('nouveau_client', function(pseudo) {
-    pseudo = ent.encode(pseudo);
+    if(pseudo!=null){
+      pseudo = ent.encode(pseudo);
+    }
     socket.pseudo = pseudo;
     socket.broadcast.emit('nouveau_client', pseudo);
 
