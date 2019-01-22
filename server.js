ddffe589e77e22d37622b55f7f50432b0c9cc62a
ROBIN Eliott <liopty@gmail.com>
2019-01-22 12:10:23 +0100

    idem

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ var ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité 
     }
     roomno.push(tempoId);
     socket.join(tempoId);
-    values = [id, FALSE];
+    values = [temId, FALSE];
     client.query(insertTableRoom, values, (err, res) => {
     if (err) throw err;
     console.log(res);
