c420ec64e100fcb587fb9379be39916ef5519336
ROBIN Eliott <liopty@gmail.com>
2019-01-22 10:31:05 +0100

    idem

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ test = res;
 console.log(res);
 
 });
-setTimeout(function(){ console.log(Object.values(test)); }, 3000);
+setTimeout(function(){ console.log(Object.values(test)[4]); }, 3000);
 
 
 const express = require("express");
