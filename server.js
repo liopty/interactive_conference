a784fc46a4479de4abd1ebd025cdfa30b344561e
ROBIN Eliott <liopty@gmail.com>
2019-01-22 09:18:49 +0100

    select table room

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ const client = new Client({
 client.connect();
 /*(code, title, did, date_prod, kind)
     VALUES ('T_601', 'Yojimbo', 106, '1961-06-16', 'Drama');*/
+/*
 client.query("INSERT INTO room VALUES (5000,FALSE);", (err, res) => {
 if (err) throw err;
 console.log(res);
 client.end();
 });
-
+*/
 client.query("SELECET * FROM room;", (err, res) => {
 if (err) throw err;
 console.log(res);
