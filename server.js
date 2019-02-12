f7ddeb842eba7399d82bf1acea9f8d1c6af0cb00
Maxime <clairand.maxime@gmail.com>
2019-02-12 15:45:17 +0100

    ext

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ var ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité 
       console.log('Logs enregistrés dans le fichier "externalize.csv"');
     });
 
-    res.download('./README.txt', 'README.txt');
+    res.download('./logs/README.txt', 'README.txt');
   });
 
   //POUR VIDER LES TABLES DE LA BD
