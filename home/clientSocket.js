75ab37daea307a211003e46825f7b405c49d3e40
ROBIN Eliott <liopty@gmail.com>
2019-02-12 18:34:43 +0100

    idem

--- home/clientSocket.js
+++ home/clientSocket.js
@@ -167,7 +167,7 @@ function insereMessage(pseudo, message,idMessage, mind) {
 }
 
 socket.on('AfficherVote', function(msgId, voteValue) {
-  document.getElementById("vote_"+msgId).innerHTML(voteValue);
+  document.getElementById("vote_2").innerHTML("test");
 });
 
 //Ajout d'un event listener sur les bouton qui ont pour class : vote
