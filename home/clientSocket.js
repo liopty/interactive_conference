cb8c74dfc6aac6f19fd7c7769a6da2b5fe1bad47
doucelin <doucelin.pedicone@gmail.com>
2019-02-13 14:17:10 +0100

    titre MAJ

--- home/clientSocket.js
+++ home/clientSocket.js
@@ -41,7 +41,7 @@ $('#creer_room').on('click', function() {
     var element = document.getElementById('id01');
     actualRoom = roomID;
     element.innerHTML = "Room n°" + actualRoom;
-    document.title = "Room "+actualRoom + ' - ' + document.title; // met la room dans l'onglet
+    document.title = "Room "+actualRoom + ' - ' + "Interactive Conference"; // met la room dans l'onglet
     closePopup();
     if(idIntoDB === null && actualRoom !== null){
       idIntoDB = userId;
