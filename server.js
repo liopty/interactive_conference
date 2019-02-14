bf2c52f3761db035d6085166ed9f9a7a179930c7
Bastien RAOUL <bastien.raoul@gmail.com>
2019-02-14 11:57:15 +0100

    commentaires

--- server.js
+++ server.js
@@ -244,8 +244,6 @@ io.on('connection', function(socket){
           socket.emit('quizz', {question : question, mind: "yes"});
         });
       });
-
-
     });
 
   socket.on('leave_room', function(idRoom){
