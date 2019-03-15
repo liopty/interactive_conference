efbb0b99eb9b2f85bcca0110e93e7f9407431564
liopty <37587821+liopty@users.noreply.github.com>
2019-03-15 11:36:56 +0100

    Update clientSocket.js

--- home/clientSocket.js
+++ home/clientSocket.js
@@ -85,9 +85,8 @@ $('#rejoindre_room').on('click', function() {
 //évènement click sur le bouton qui appel la fontion 'quitter_room'
 $('#quitter_room').on('click', function() {
   document.getElementById('messages').innerHTML = ""; // On efface les messages pour le client qui veut quitter le salon
-  console.log(document.getElementById('activeOnglet1'));  
-  //document.getElementById('activeOnglet1').classList.add("mdc-tab--active active");
-  //document.getElementById('activeOnglet2').classList.remove("mdc-tab--active active");
+  document.getElementById('activeOnglet1').classList.add("mdc-tab--active", "active");
+  document.getElementById('activeOnglet2').classList.remove("mdc-tab--active", "active");
   socket.emit('leave_room', actualRoom);
   actualRoom = null; //On dit que le client n'est plus dans un salon
   idIntoDB = null; // On dit que le client n''a plus d'identifiant
